135fc49fc6308da386061b91a647298476622ea7
Oguzhan Elkapmis <oguzhan.elkapmis@gmail.com>
2022-03-20 15:50:06 +0300

    Add files via upload
    https://app.patika.dev/moduller/java101/pratik-array-letter

---
+++ HarfYazdirma.java
@@ -0,0 +1,28 @@
+//  https://app.patika.dev/moduller/java101/pratik-array-letter
+
+public class HarfYazdirma {
+    public static void main(String[] args) {
+        String[][] letter = new String[7][4];
+
+        for (int i = 0; i < letter.length; i++) {
+            for (int j = 0; j < letter[i].length; j++) {
+                if (j == 0) {
+                    letter[i][j] = " ♥ ";
+                } else if ((j == 1 || j == 2) && (i % 3 == 0)) {
+                    letter[i][j] = " ♥ ";
+                } else if (j == 3 && (i % 3 != 0)) {
+                    letter[i][j] = " ♥ ";
+                } else {
+                    letter[i][j] = "   ";
+                }
+            }
+        }
+
+        for (String[] row : letter){
+            for (String col : row){
+                System.out.print(col);
+            }
+            System.out.println();
+        }
+    }
+}
