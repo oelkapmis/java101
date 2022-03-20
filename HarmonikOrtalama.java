51bf689a0fe838406619bd5552c166ab9c67b4db
Oguzhan Elkapmis <oguzhan.elkapmis@gmail.com>
2022-03-20 14:54:23 +0300

    Add files via upload

---
+++ HarmonikOrtalama.java
@@ -0,0 +1,26 @@
+//  https://app.patika.dev/moduller/java101/pratik-array-ortalama
+
+public class HarmonikOrtalama {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5};
+        int sum = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            sum += numbers[i];
+        }
+        double harmonikSeri = harmonikSeri(numbers.length);
+        double harmonikOrt = numbers.length / harmonikSeri;
+
+        System.out.println("Aritmetik ortalama: " + sum / numbers.length);
+        System.out.println("Harmonik seri: " + harmonikSeri);
+        System.out.println("Harmonik ortalama: " + harmonikOrt);
+    }
+
+    private static double harmonikSeri(int n) {
+        double harmonikSeri = 0;
+        for(int i = 1; i <= n; i++){
+            harmonikSeri += 1.0 / (i);
+        }
+        return harmonikSeri;
+    }
+}
+
